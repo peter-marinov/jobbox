ed16da543884d0a9b37af2f0953869164f341f8b
peter-marinov <peter.marinov91@gmail.com>
2023-08-11 14:00:02 +0300

    Changes for debug false
    Debug:
    - it is now set to false
    
    whitenoise:
    - it is now installed and added in the apps and in the middlewares. With it the static is loaded when debug is false

--- jobbox/settings.py
+++ jobbox/settings.py
@@ -25,14 +25,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)v&()8n6lsja5-ollw117y5%9+qfoyk5!91f!$_ougpxb7t&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'local']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,9 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static_files'
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static_files'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static_files'
 
 
 # Default primary key field type
